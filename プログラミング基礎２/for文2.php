3417035cdd94768a0ed05e812b4f60444e431dc8
北川　大公 <kitakita19881022@gmail.com>
2017-04-10 17:58:30 +0900

    for文2　完了

---
+++ プログラミング基礎２/for文2.php
@@ -0,0 +1,14 @@
+<?php
+// // $num = 1;
+// for($i= 0 ; $i < 20 ; $i ++) {
+//   $num = $num * 8;
+// }
+// echo $nm;
+// echo "<br>";
+
+for($i= 0 ; $i < 30 ; $i ++) {
+  $renketu .= "A";
+}
+
+echo $renketu;
+echo "<br>";
