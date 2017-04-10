65e7d8d981e6bd0718fa2a8ada77d05992c30279
北川　大公 <kitakita19881022@gmail.com>
2017-04-10 16:26:33 +0900

    完了

---
+++ プログラミング基礎２/switch文.php
@@ -0,0 +1,18 @@
+<?php
+ $num = 2;
+ switch($num)
+  {
+   case 1:
+   echo  "one";
+   break;
+
+   case 2:
+   echo  "two";
+   break;
+
+   default:
+   echo  "想定外";
+   break;
+  }
+
+  echo "<br>";
---
+++ プログラミング基礎２/switch文２.php
@@ -0,0 +1,16 @@
+<?php
+
+$type = A;
+switch($type)
+ {
+  case A:
+  echo  "英語";
+  break;
+
+  case あ:
+  echo  "日本語";
+  break;
+
+ }
+
+ echo "<br>";
