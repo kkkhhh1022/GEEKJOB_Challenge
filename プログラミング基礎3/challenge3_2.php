3da8238a919974ffa8a0feb3b94e90660d8dc9e3
北川　大公 <kitakita19881022@gmail.com>
2017-04-12 18:50:33 +0900

    引数１　完了

---
+++ プログラミング基礎3/challenge3_2.php
@@ -0,0 +1,21 @@
+<?php
+function hanbetsu($num1,$num2,$num3) {
+   if ($num1%2 ==0) {
+           print "偶数<br/>";
+   }else {
+           print "奇数<br/>";
+   }
+   if ($num2%2 ==0) {
+           print "偶数<br/>";
+   }else {
+           print "奇数<br/>";
+   }
+   if ($num3%2 ==0) {
+           print "偶数<br/>";
+   }else {
+           print "奇数<br/>";
+   }
+
+}
+
+print hanbetsu(2,3,6);
