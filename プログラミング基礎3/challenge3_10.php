acb17bb710d64de7d835b48c0aa3e01319845ad9
北川　大公 <kitakita19881022@gmail.com>
2017-04-14 16:10:47 +0900

    continueとbreak　完了

---
+++ プログラミング基礎3/challenge3_10.php
@@ -0,0 +1,22 @@
+<?php
+function data($id){
+	$kitagawa = array(1, 'きたがわ', '昭和63年10月22日', '神奈川');
+	$ito = array(2, 'いとう', '平成2年6月3日', null);
+	$yamada = array(3, 'やまだ', '平成4年5月5日', '千葉県');
+		$member = array($kitagawa, $ito, $yamada);
+		return $member[$id - 1];
+}
+$limit = 2;
+$array = array(1, 2, 3);
+foreach($array as $id){
+	$array_data = data($id);
+	foreach($array_data as $key => $value){
+		if($key == 0 || $value == null){
+			continue;
+		}
+		echo "$value <br>";
+	}
+  if($id == $limit) {
+      break;
+  }
+}
