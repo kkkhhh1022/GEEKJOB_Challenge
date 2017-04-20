37d7e467413eec145caf930d025add57ca82dee6
北川　大公 <kitakita19881022@gmail.com>
2017-04-20 18:00:02 +0900

    HTMLのタグについて　完了

---
+++ データ操作/challenge5_6.php
@@ -0,0 +1,8 @@
+<html>
+
+   信号機の色は、
+   <span style="background-color:#0099ff">青色</span>
+   <span style="background-color:#ffff00">黄色</span>
+   <span style="background-color:#ff0000">赤色</span>です。
+
+</html>
